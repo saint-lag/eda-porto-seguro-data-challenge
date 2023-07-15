03141958b0c9bb1c8e913160c686a16a56849cd9
Gabriel Maia <maiasena.gabriel@gmail.com>
2023-07-15 01:26:15 -0300

    Bomb commit
    This commit is actually a bomb

--- main.R
+++ main.R
@@ -2,6 +2,12 @@
 library(ggplot2)
 library(corrplot)
 library(outliers)
+library(corrr)
+library(ggcorrplot)
+library(factoextra)
+library(openxlsx)
+library(writexl)
+library(aplpack)
 
 # Importing data
 metadata=read.csv("metadata.csv")
@@ -45,6 +51,11 @@ y_missing_values <- sum(is.na(y_column_data))
 
 missing_values_list[["y"]] <- y_missing_values
 
+# Removing 'id' column
+train_without_id <- train
+train_without_id['id'] <- NULL
+
+
 ## Data Distribution:
 
 # Creating a new dataframe for each data type
@@ -58,6 +69,12 @@ quali_nom_list <- list()
 quali_ord_list <- list()
 quant_disc_list <- list()
 quant_cont_list <- list()
+
+# Dataframes:
+quant_cont_df <- data.frame(quant_cont_list)
+quant_disc_df <- data.frame(quant_disc_list)
+quali_nom_df <- data.frame(quali_nom_list)
+quali_ord_df <- data.frame(quali_nom_list)
 
 create_list_for_df <- function(df) {
   col_index <- 1
@@ -97,6 +114,70 @@ quant_cont_summary <- list()
 for(i in seq(length(quant_cont_list))) {
   quant_cont_summary[[i]] <- summary(quant_cont_list[[i]])
 }
+
+## Summaries
+sd_train <- sapply(train_without_id, sd)
+summary_train <- summary(train_without_id)
+
+# write.xlsx(summary_train, file = "train_summary.xlsx")
+# write_xlsx(data.frame(sd_train), path = "sd_train.xlsx")
+
+means <- colMeans(train_without_id)
+minimums <- -apply(train_without_id, 2, min)
+maximums <- apply(train_without_id, 2, max)
+sd_values <- apply(train_without_id, 2, sd)
+
+quali_nom_means <- colMeans(quali_nom_df)
+quali_nom_minimums <- -apply(quali_nom_df, 2, min)
+quali_nom_maximums <- apply(quali_nom_df, 2, max)
+quali_nom_sd_values <- apply(quali_nom_df, 2, sd)
+
+quant_cont_means <- colMeans(quant_cont_df)
+quant_cont_minimums <- -apply(quant_cont_df, 2, min)
+quant_cont_maximums <- apply(quant_cont_df, 2, max)
+quant_cont_sd_values <- apply(quant_cont_df, 2, sd)
+
+quant_disc_means <- colMeans(quant_disc_df)
+quant_disc_minimums <- -apply(quant_disc_df, 2, min)
+quant_disc_maximums <- apply(quant_disc_df, 2, max)
+quant_disc_sd_values <- apply(quant_disc_df, 2, sd)
+
+quali_ord_means <- colMeans(quali_ord_df)
+quali_ord_minimums <- -apply(quali_ord_df, 2, min)
+quali_ord_maximums <- apply(quali_ord_df, 2, max)
+quali_ord_sd_values <- apply(quali_ord_df, 2, sd)
+
+barplot(rbind(quali_nom_means, quali_nom_minimums, quali_nom_maximums, quali_nom_sd_values),
+        beside = TRUE,
+        names.arg = colnames(quali_nom_df),
+        legend.text = c("Mean", "Minimum", "Maximum", "Standard Deviation"),
+        col = c("steelblue", "orange", "green", "red"),
+        ylab = "Values",
+        main = "Summary Statistics of Variables (Qualitative Nominal)")
+
+barplot(rbind(quant_cont_means, quant_cont_minimums, quant_cont_maximums, quant_cont_sd_values),
+        beside = TRUE,
+        names.arg = colnames(quant_cont_df),
+        legend.text = c("Mean", "Minimum", "Maximum", "Standard Deviation"),
+        col = c("steelblue", "orange", "green", "red"),
+        ylab = "Values",
+        main = "Summary Statistics of Variables (Quantitative Continuos)")
+
+barplot(rbind(quant_disc_means, quant_disc_minimums, quant_disc_maximums, quant_disc_sd_values),
+        beside = TRUE,
+        names.arg = colnames(quant_disc_df),
+        legend.text = c("Mean", "Minimum", "Maximum", "Standard Deviation"),
+        col = c("steelblue", "orange", "green", "red"),
+        ylab = "Values",
+        main = "Summary Statistics of Variables (Quantitative Discrete)")
+
+barplot(rbind(quali_ord_means, quali_ord_minimums, quali_ord_maximums, quali_ord_sd_values),
+        beside = TRUE,
+        names.arg = colnames(quali_ord_df),
+        legend.text = c("Mean", "Minimum", "Maximum", "Standard Deviation"),
+        col = c("steelblue", "orange", "green", "red"),
+        ylab = "Values",
+        main = "Summary Statistics of Variables (Qualitative Ordinal)")
 
 ## Pair Correlation:
 
@@ -147,7 +228,40 @@ corrplot.mixed(cor_matrix_quant_cont, tl.col="black", tl.pos="lt")
 corrplot.mixed(cor_matrix_quant_disc, tl.col="black", tl.pos="lt")
 corrplot.mixed(cor_matrix_quali_nom, tl.col="black", tl.pos="lt")
 
-  
+## PCA
+# Normalization
+train_normalized <- scale(train_without_id)
+quali_nom_normalized <- scale(quali_nom_df)
+quali_ord_normalized <- scale(quali_ord_df)
+quant_cont_normalized <- scale(quant_cont_df)
+quant_disc_normalized <- scale(quant_disc_df)
+
+
+# PCA: Correlation matrix
+corr_train_matrix <- cor(train_normalized)
+ggcorrplot(corr_matrix)
+
+# Applying PCA
+data.pca <- princomp(corr_matrix)
+summary(data.pca)
+fviz_eig(data.pca, addlabels=TRUE)
+
+# PCA: Biplot
+fviz_pca_var(data.pca, col.var = "black", repel=TRUE)
+
+# Contribution of each variable
+num_vars = 23 # One third of the total of vars[69/3]
+
+# 10 components, since they add up to 87% of relevance 
+dim = 10
+fviz_cos2(data.pca, top = num_vars, choice = "var", axes = 1:dim)
+fviz_cos2(data.pca, top = num_vars, choice = "var", axes = 1:2)
+
+# Biplot & cos2
+fviz_pca_var(data.pca, col.var = 'cos2',
+             gradient.cols = c('blue', 'purple', 'magenta'),
+             repel = TRUE)
+
 ## Utils
 column_data_list_length <-length(column_data_list)
 
